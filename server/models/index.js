b8681eaa173d89f7529e9c4372ede30363912f6f
Misael Reyes <misaelrey90@gmail.com>
2021-10-12 12:30:55 -0500

    product model

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,4 @@
 const User = require('./User');
-const Thought = require('./Thought');
+const Thought = require('./Product');
 
-module.exports = { User, Thought };
+module.exports = { User, Project };
