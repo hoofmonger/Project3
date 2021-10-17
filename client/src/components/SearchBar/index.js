97c3f3d365e593e0fa8e0dee7d7d0149d0c0b83f
James Hoofard <james.hoofard.iii@gmail.com>
2021-10-17 13:47:23 -0500

    improved the code's look

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,45 +1,18 @@
-import React, {useRef} from "react";
-// import { Link } from 'react-router-dom';
-import { useQuery } from "@apollo/client";
-// import { SearchResults } from '../../pages/SearchResults';
-import { QUERY_SEARCH, QUERY_PRODUCTS } from "../../utils/queries";
+import React from "react";
 
-
-const SearchBar = ({handleSearchChange}) => {
-  // const { data } = useQuery(QUERY_PRODUCTS);
-  // console.log(data)
-
-
+const SearchBar = ({ handleSearchChange }) => {
   const handleClick = (event) => {
     event.preventDefault();
     console.log("test");
-    
+
     try {
-      // const { data } = searchResults({});
-      // console.log(data);
-      // window.location.reload();
     } catch (error) {
       console.error(error);
     }
-    // class NameForm extends React.SearchBar {
-    //   constructor(props) {
-    //     super(props);
-    //     this.state = {value: ''};
-    //     this.handleChange = this.handleChange.bind(this);
-    //     this.handleSubmit = this.handleSubmit.bind(this);
-    //   }
-    
-    //   handleChange(event) {    this.setState({value: event.target.value});  }
-    //   handleSubmit(event) {
-    //     alert('A name was submitted: ' + this.state.value);
-    //     event.preventDefault();
-    //   }
-    
   };
 
   return (
     <form action="/" method="get">
-      {/* <Link to= {`/SearchResults/${product.name}`}> */}
       <label htmlFor="header-search">
         <span className="visuallyhidden">Search Products</span>
       </label>
@@ -48,11 +21,9 @@ const SearchBar = ({handleSearchChange}) => {
         id="header-search"
         placeholder="Search Products"
         name="s"
-        // ref = {searchQuery}
-        onChange = {handleSearchChange}
+        onChange={handleSearchChange}
       />
       <button onClick={handleClick}>Search!</button>
-      {/* </Link> */}
     </form>
   );
 };
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useQuery } from "@apollo/client";
 
 import SearchBar from "../components/SearchBar";
@@ -6,55 +6,31 @@ import SearchBar from "../components/SearchBar";
 import { QUERY_PRODUCTS } from "../utils/queries";
 import Product from "../components/Product";
 
-
 const Home = () => {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
   const products = data?.products || [];
-  const [searchQuery, setSearchQuery] = useState('')
-console.log("products on home page?", products)
+  const [searchQuery, setSearchQuery] = useState("");
+  console.log("products on home page?", products);
 
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+    console.log(searchQuery);
+  };
 
-const handleSearchChange = (e)=>{
-  setSearchQuery(e.target.value)
-  console.log(searchQuery)
-}
-
-function filtering(){
-  if (!products){
-    console.log("no products found ")
-  } 
-  else {
-    const filteredProducts = products.filter((product)=>{
-      return product.name.includes(searchQuery) 
-    });
-    return filteredProducts
-    // class NameForm extends React.SearchBar {
-    //   constructor(props) {
-    //     super(props);
-    //     this.state = {value: ''};
-    
-    //     this.handleChange = this.handleChange.bind(this);
-    //   }
-    
-    //   handleChange(event) {
-    //     this.setState({value: event.target.value});
-    //   }
-    
-    
-  // else if (products.filter(product => {if (product.name != "whatever your input on submital is"){
-  //   console.log("woo no product")}
-  // } 
-  // else {
-  //   return products.filter(product => product.name === )
-  // } 
+  function filtering() {
+    if (!products) {
+      console.log("no products found ");
+    } else {
+      const filteredProducts = products.filter((product) => {
+        return product.name.toLowerCase().includes(searchQuery.toLowerCase());
+      });
+      return filteredProducts;
+    }
   }
-}
   return (
     <main>
       <h2>Products</h2>
-      <SearchBar
-      handleSearchChange = {handleSearchChange}
-       />
+      <SearchBar handleSearchChange={handleSearchChange} />
       <div className="flex-row justify-center">
         <div
           className="col-12 col-md-10 mb-3 p-3"
@@ -63,20 +39,20 @@ function filtering(){
       </div>
 
       {products.length ? (
-          <div className="flex-row">
-            {filtering().map((product) => (
-              <Product
-                key={product._id}
-                // _id={product._id}
-                name={product.name}
-                price={product.price}
-                quantity={product.quantity}
-              />
-            ))}
-          </div>
-        ) : (
-          <h3>No products by that name. Sorry!</h3>
-        )}
+        <div className="flex-row">
+          {filtering().map((product) => (
+            <Product
+              key={product._id}
+              // _id={product._id}
+              name={product.name}
+              price={product.price}
+              quantity={product.quantity}
+            />
+          ))}
+        </div>
+      ) : (
+        <h3>No products by that name. Sorry!</h3>
+      )}
     </main>
   );
 };
